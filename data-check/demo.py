ae97fc3d1276cbb3b41cbfe89c2ba8d50bda7c19
Alexsandro Viana de Abreu <viana.abreu@outlook.com>
2023-05-27 15:12:40 +0200

    adding the demo file

--- data-check/demo.py
+++ data-check/demo.py
@@ -1,9 +1,7 @@
 """Dummy challenge for Kitt Demo"""
 
-import math
+
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    if radius > 0:
-        return radius * radius * math.pi
-    return 0
+    pass  # YOUR CODE HERE
